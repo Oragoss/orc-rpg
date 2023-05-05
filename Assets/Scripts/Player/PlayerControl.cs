7002eaf03f2d49dfdc9330fdb2489d7da0b06a28
Jon Sousa <jonathonsousa@gmail.com>
2023-05-04 20:26:59 -0500

    Collectibles move towards the player

--- Assets/Scripts/Player/PlayerControl.cs
+++ Assets/Scripts/Player/PlayerControl.cs
@@ -8,16 +8,65 @@ using UnityEngine.Tilemaps;
 public class PlayerControl : MonoBehaviour
 {
     [SerializeField] float speed = 5.0f;
+    [SerializeField] LayerMask ignoreLayerMask;
 
     Rigidbody2D rb;
 
     private void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
-        speed = speed * 10;
     }
 
     private void Update()
+    {
+        ClickOnObject();
+    }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        TakeDamage(collision);
+        PickUpCollectable(collision);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        PullItemsToPlayer(collision);
+    }
+
+    //TODO: Create trigger that's larger than the player and "pulls" items towards the player
+    private void PullItemsToPlayer(Collider2D collision)
+    {
+        var go = collision.gameObject;
+        if (go.CompareTag("Collectable"))
+        {
+            Debug.Log("Pulling " + go.name);
+            Vector3 pos = transform.position - go.transform.position;
+            go.GetComponent<Rigidbody2D>().velocity = pos.normalized * 5;
+        }
+    }
+
+    
+    private void PickUpCollectable(Collision2D collision)
+    {
+        var go = collision.gameObject;
+        if (go.CompareTag("Collectable"))
+        {
+            Debug.Log("Picked up " + go.name + " it's a " + go.GetComponent<CollectableType>().type);
+            Destroy(go);
+        }
+    }
+
+    private void TakeDamage(Collision2D collision)
+    {
+        
+    }
+
+    private void Movement()
     {
         float vertical = Input.GetAxisRaw("Vertical") * speed;
         float horizontal = Input.GetAxisRaw("Horizontal") * speed;
@@ -25,24 +74,26 @@ public class PlayerControl : MonoBehaviour
         Vector2 movement = new Vector2(horizontal * speed, vertical * speed);
         movement *= Time.deltaTime;
         rb.velocity = movement;
+    }
 
+    private void ClickOnObject()
+    {
         //TODO: Implement a cooldown, Should holding down increase attack animations?
         if (Input.GetButtonDown("Fire1"))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, ~ignoreLayerMask);
+            Physics2D.queriesHitTriggers = false;
 
             if (hit.collider != null)
             {
-                Debug.Log("CLICKED " + hit.collider.name);
+                HarvestResource(hit.collider.gameObject);
             }
-            ClickOnObject(hit.collider.gameObject);
         }
     }
 
-    private void ClickOnObject(GameObject go)
+    private void HarvestResource(GameObject go)
     {
-        //TODO: Determine what kind of object it is
         if (Vector3.Distance(transform.position, go.transform.position) <= 2 && !go.CompareTag("Player"))
         {
             if (go.CompareTag("Wood") || go.CompareTag("Stone"))
@@ -50,7 +101,6 @@ public class PlayerControl : MonoBehaviour
                 go.GetComponent<GenerateCollectable>().Generate();
                 Destroy(go);
             }
-
         }
     }
 }
---
+++ Assets/Scripts/Resources/CollectableType.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Resources
+{
+    public class CollectableType : MonoBehaviour
+    {
+        public enum Collectable
+        {
+            Wood,
+            Stone,
+            Option3,
+            Option4,
+            Option5,
+        }
+        public Collectable type;
+    }
+}
